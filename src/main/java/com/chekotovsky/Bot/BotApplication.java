2502a2f823973a02c0f16526880cfc281508d335
Andrey Checkotovsky <checkotovskyandrey@gmail.com>
2023-08-14 13:27:56 +0300

    Custom commands for tasks were added

--- src/main/java/com/chekotovsky/Bot/BotApplication.java
+++ src/main/java/com/chekotovsky/Bot/BotApplication.java
@@ -3,6 +3,9 @@ package com.chekotovsky.Bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Developed by
+// Andrey Chekotovsky
+
 @SpringBootApplication
 public class BotApplication {
 
